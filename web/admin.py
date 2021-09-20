fd5eee71034396717521cacfebaed01dc0dabec4
solokhovir <sir1504@mail.ru>
2021-09-20 23:43:45 +0300

    Changes 20.09.2021

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Analyses, Patients, Reports
+from .models import Analyses, Patients, Reports, Sex
 
 admin.site.register(Analyses)
 admin.site.register(Patients)
-
+admin.site.register(Sex)
