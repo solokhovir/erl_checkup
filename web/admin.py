06003b14c98e14680c1237b1617cda5bccd56755
solokhovir <sir1504@mail.ru>
2021-09-21 00:20:01 +0300

    Changes 20.09.2021

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Analyses, Patients, Reports, Sex
+from .models import Analyses, Patients, Sex
 
 admin.site.register(Analyses)
 admin.site.register(Patients)
--- web/models.py
+++ web/models.py
@@ -5,13 +5,20 @@ from django.db import models
 class Sex(models.Model):
     sex_main = models.CharField('Пол', max_length=20)
 
+    class Meta:
+        verbose_name_plural = 'Пол'
+        verbose_name = 'Пол'
+
+    def __str__(self):
+        return self.sex_main
+
 
 # Создание базы данных с пациентами
 class Patients(models.Model):
     name = models.CharField('ФИО', max_length=1000)
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', null=True)
     sex = models.ForeignKey(Sex, on_delete=models.CASCADE)
-    is_confirm_consent = models.BooleanField('Согласие на обработку данных')
+    is_confirm_consent = models.BooleanField('Согласие на обработку данных', default=False)
     email = models.EmailField('e-mail')
     phone = models.CharField('Телефон', max_length=12)
 
@@ -26,24 +33,28 @@ class Patients(models.Model):
 
 # Создание базы данных с анализами
 class Analyses(models.Model):
-    taken_date = models.DateField
-    group_number = models.IntegerField
+    taken_date = models.DateField('Дата исследования', null=True)
+    group_number = models.IntegerField()
     patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
 
-    leukocyte = models.CharField
-    lymphocytes = models.FloatField
-    monocytes = models.FloatField
-    basophils = models.FloatField
-    eosinophils = models.FloatField
-    neutrophils = models.CharField
-    young = models.CharField
-    myelocytes = models.CharField
-    toxic_grit = models.CharField
-    plasma = models.CharField
+    leukocyte = models.FloatField('Лейкоциты - WBC')
+    lymphocytes = models.FloatField('Лимфоциты - LYM%')
+    monocytes = models.FloatField('Моноциты - MON%')
+    basophils = models.FloatField('Базофилы - BAS%')
+    eosinophils = models.FloatField('Эозинофилы - EOS%')
+    neutrophils = models.FloatField('Нейтрофилы - NEUT%')
+    young = models.FloatField('Юные - IMM%')
+    myelocytes = models.FloatField('Миелоциты')
+    toxic_grit = models.FloatField('Токсическая зернистость')
+    plasma = models.FloatField('Плазматические клетки')
 
-    stab = models.CharField
-    segmented = models.CharField
+    stab = models.IntegerField()
+    segmented = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Анализы'
+        verbose_name = 'Анализ'
 
 
-class Reports(models.Model):
-    report = models.ForeignKey(Analyses, on_delete=models.CASCADE)
+# class Reports(models.Model):
+#     report = models.ForeignKey(Analyses, on_delete=models.CASCADE)
